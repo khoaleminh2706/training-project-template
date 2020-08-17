1afac15cc2855825d05d3102ba9e39a21463e336
Khoa Le Minh <khoa.le.minh@preciofishbone.se>
2020-08-17 18:11:54 +0700

    Open modal bằng Javascript, không dùng btn

--- src/scripts/pages/home-page.ts
+++ src/scripts/pages/home-page.ts
@@ -7,12 +7,27 @@ import ready from '../utilities/_helper';
 import FileService from '../service/_fileService';
 import tableRow from '../components/_tableRow';
 
+let modal = document.getElementById('file-modal');
+
 ready(() => {
+  document
+    .getElementById('btn-file-moi')
+    ?.addEventListener('click', () => openModal('file'));
+  document
+    .getElementById('btn-folder-moi')
+    ?.addEventListener('click', () => openModal('folder'));
+
   const service = new FileService();
-  let point = document.querySelector('#doc-list-body');
+  let point = document.getElementById('doc-list-body');
 
   if (point !== null)
     point.innerHTML += tableRow(
       service.getData() as Array<IBaseModel>,
     );
 });
+
+function openModal(task: string) {
+  if (modal !== null) {
+    modal.style.display = 'block';
+  }
+}
