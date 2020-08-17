ea5238a0883149ba1fbbe19690b7af0ece6fc86f
Khoa Le Minh <khoa.le.minh@preciofishbone.se>
2020-08-17 09:36:45 +0700

    add modal to page

--- src/scripts/pages/home-page.ts
+++ src/scripts/pages/home-page.ts
@@ -1,6 +1,7 @@
 import 'bootstrap/js/dist/util';
 import 'bootstrap/js/dist/button';
 import 'bootstrap/js/dist/collapse';
+import 'bootstrap/js/dist/modal';
 
 import ready from '../utilities/_helper';
 
