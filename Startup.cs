94246fd51a2ef22db4bcbb406eb80230e2da964a
Khoa Le Minh <khoa.le.minh@preciofishbone.se>
2020-08-24 18:05:30 +0700

    end of 24/8

--- Startup.cs
+++ Startup.cs
@@ -1,55 +1,87 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authentication.AzureAD.UI;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Authorization;
 
 namespace FileServer
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddRazorPages();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
-
-            app.UseDefaultFiles();
-            
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapRazorPages();
-            //});
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddRazorPages();
+      services.Configure<CookiePolicyOptions>(options =>
+        {
+          // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+          options.CheckConsentNeeded = context => true;
+          options.MinimumSameSitePolicy = SameSiteMode.None;
+        });
+
+      services.AddAuthentication(AzureADDefaults.AuthenticationScheme)
+              .AddAzureAD(options => Configuration.Bind("AzureAd", options));
+
+      services.Configure<OpenIdConnectOptions>(AzureADDefaults.OpenIdScheme, options =>
+      {
+        options.Authority = options.Authority + "/v2.0/";         // Microsoft identity platform
+
+        options.TokenValidationParameters.ValidateIssuer = false; // accept several tenants (here simplified)
+      });
+
+      services.AddMvc(options =>
+      {
+        // options.EnableEndpointRouting = false;
+        var policy = new AuthorizationPolicyBuilder()
+                 .RequireAuthenticatedUser()
+                 .Build();
+        options.Filters.Add(new AuthorizeFilter(policy));
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Error");
+      }
+
+      // app.Use((context, next) =>
+      // {
+      //   context.Request.Scheme = "https";
+      //   return next();
+      // });
+
+      // app.UseHttpsRedirection();
+      app.UseRouting();
+      app.UseStaticFiles();
+
+      app.UseCookiePolicy();
+      app.UseAuthentication();
+      // app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapRazorPages();
+      });
+    }
+  }
 }
